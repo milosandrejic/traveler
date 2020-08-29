7b7c00a1ba78ee5230cfd7a347d1db4a00605f2f
Milos Andrejic <milosandrejic1993@gmail.com>
2020-08-29 14:34:57 +0200

    fixed popup and hamburger color

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
-
 const mobileNav = document.querySelector('.mobile-nav');
 const hamburgerCheckbox = document.querySelector('.hamburger-checkbox');
+const headerHeight = document.querySelector('.header').scrollHeight;
+const hamburger = document.querySelector('.hamburger-content');
+const footer = document.querySelector('footer');
+const docHeight = document.body.scrollHeight;
+const footerHeightFromTop = docHeight - footer.scrollHeight;
+
 
 mobileNav.addEventListener('click', () => {
   if(event.target.matches('.mobile-nav__link')){
     hamburgerCheckbox.checked=false;
   }
 });
+
+
+
+document.addEventListener('scroll', () => {
+  if(window.pageYOffset < headerHeight - 150 || window.pageYOffset > footerHeightFromTop){
+    hamburger.classList.add('color-white')
+    hamburger.classList.remove('color-blue')
+  }
+  else {
+    hamburger.classList.remove('color-white')
+    hamburger.classList.add('color-blue')
+  }
+})
+
+const setHamburgerColor = () => {
+  if(window.pageYOffset < headerHeight - 150 || window.pageYOffset > footerHeightFromTop){
+    hamburger.classList.add('color-white')
+    hamburger.classList.remove('color-blue')
+  }
+  else {
+    hamburger.classList.remove('color-white')
+    hamburger.classList.add('color-blue')
+  }
+};
+
+setHamburgerColor();
